d425ad0002b8d3101fa98ab3779edd2ef663e715
Sven Mkw <sven.mkw@gmail.com>
2015-04-24 20:10:35 +0200

    Initial commit

---
+++ Assignment1-KinectTouchpad/Application.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+
+class DepthCamera;
+class KinectMotor;
+
+class Application
+{
+public:
+	Application();
+	virtual ~Application();
+
+	void loop();
+
+	void processFrame();
+
+	void makeScreenshots();
+	void clearOutputImage();
+
+	bool isFinished();
+
+
+protected:
+	DepthCamera *m_depthCamera;
+	KinectMotor *m_kinectMotor;
+
+	cv::Mat m_bgrImage;
+	cv::Mat m_depthImage;
+	cv::Mat m_outputImage;
+    cv::Mat reference_image;
+    
+    bool set_reference_image = false;
+	bool m_isFinished;
+};
